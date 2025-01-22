7f748af18006eb1a05dcdf33feb6dc85b00fb957
Martin Hickey <martin.hickey@ie.ibm.com>
2025-01-22 17:45:58 +0000

    Need to specify types for collection assignment
    Fixes:
    
    fms_mo/aiu_addons/i8i8/i8i8_aiu_adapter.py:66: error: Unsupported target for indexed assignment ("Mapping[str, Any]")  [index]
    fms_mo/aiu_addons/i8i8/i8i8_aiu_adapter.py:67: error: Unsupported target for indexed assignment ("Mapping[str, Any]")  [index]
    fms_mo/aiu_addons/i8i8/i8i8_aiu_adapter.py:75: error: Unsupported target for indexed assignment ("Mapping[str, Any]")  [index]
    fms_mo/aiu_addons/i8i8/i8i8_aiu_adapter.py:82: error: Unsupported target for indexed assignment ("Mapping[str, Any]")  [index]
    fms_mo/aiu_addons/i8i8/i8i8_aiu_adapter.py:87: error: Unsupported target for indexed assignment ("Mapping[str, Any]")  [index]
    
    fms_mo/quant/quantizers.py:4161: error: Incompatible types in assignment (expression has type "list[int]", variable has type "None")  [assignment]
    fms_mo/quant/quantizers.py:4161: error: Argument 1 to "len" has incompatible type "None"; expected "Sized"  [arg-type]
    fms_mo/quant/quantizers.py:4162: error: Unsupported target for indexed assignment ("None")  [index]
    fms_mo/quant/quantizers.py:4162: error: Value of type "None" is not indexable  [index]
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- fms_mo/quant/quantizers.py
+++ fms_mo/quant/quantizers.py
@@ -4035,8 +4035,8 @@ class AdaRoundQuantizer(nn.Module):
         self.reset_ReSig_param(multimodal)
 
         self.beta = 2 / 3
-        self.Wshape = None
-        self.reshape2 = None
+        self.Wshape: list[Any] = list()
+        self.reshape2: list[Any] = list()
 
     def forward(self, x):
         if self.useSAWB:
