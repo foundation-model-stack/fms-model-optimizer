enable hadamard rotation, also fix a minor Qbmm bug
Signed-off-by: cliu-us <cliu@us.ibm.com>

fms_mo/modules/linear.py
@@ -36,6 +36,7 @@ from fms_mo.quant.quantizers import (
     get_weight_quantizer,
     mask_fc_kij,
 )
+from fms_mo.quant.rotation import RotQuantWrapper
 from fms_mo.utils.import_utils import available_packages
 
 if available_packages["triton"]:
@@ -158,8 +159,10 @@ class QLinear(nn.Linear):
 
         self.calib_iterator = []
         # To simplify update of clipvals in forward()
-        self.quantize_feature = Qbypass()
-        self.quantize_calib_feature = Qbypass()
+        quantA_default = Qbypass() if "rot_" not in self.qa_mode else RotQuantWrapper()
+        quantW_default = Qbypass() if "rot_" not in self.qw_mode else RotQuantWrapper()
+        self.quantize_feature = quantA_default
+        self.quantize_calib_feature = quantA_default
         if self.num_bits_feature not in [32, 16]:
             self.quantize_feature = get_activation_quantizer(
                 self.qa_mode,
@@ -187,8 +190,8 @@ class QLinear(nn.Linear):
                     quantizer2sync=self.quantize_feature,
                 )
 
-        self.quantize_weight = Qbypass()
-        self.quantize_calib_weight = Qbypass()
+        self.quantize_weight = quantW_default
+        self.quantize_calib_weight = quantW_default
         if self.num_bits_weight not in [32, 16]:
             self.quantize_weight = get_weight_quantizer(
                 self.qw_mode,

fms_mo/quant/quantizers.py
@@ -77,7 +77,7 @@ def get_activation_quantizer(
     use_rot = False
     if "rot_" in qa_mode or "_rot" in qa_mode:
         use_rot = True
-        qa_mode.replace("rot_", "").replace("_rot", "")
+        qa_mode = qa_mode.replace("rot_", "").replace("_rot", "")
 
     if not use_swcap:
         QPACTLUT = {
@@ -134,22 +134,26 @@ def get_activation_quantizer(
             )
         elif qa_mode == "dorefa":
             act_quantizer = dorefa_quantize_activation
-        elif (
-            qa_mode == "max"
-        ):  # NOTE Need to be careful using this for activation, particular to 1 sided.
-            act_quantizer = Qmax(nbits, align_zero=align_zero, minmax=False)
-        elif qa_mode == "minmax":
-            act_quantizer = Qmax(nbits, align_zero=align_zero, minmax=True)
+        elif "max" in qa_mode:
+            # NOTE Need to be careful using this for activation, particular to 1 sided.
+            if "min" in qa_mode:
+                act_quantizer = Qmax(nbits, align_zero=align_zero, minmax=True)
+            elif "pertoken" in qa_mode or "perToken" in qa_mode:
+                act_quantizer = QMaxDynamic(nbits, dim=-1)
+            elif "per_channel" in qa_mode or "perCh" in qa_mode:
+                act_quantizer = QMaxDynamic(nbits, dim=-2)
+            elif "sym" in qa_mode:
+                act_quantizer = Qmax(
+                    nbits,
+                    align_zero=True,
+                    minmax=False,
+                    extend_act_range=extend_act_range,
+                )
+            else:
+                act_quantizer = Qmax(nbits, align_zero=align_zero, minmax=False)
         elif qa_mode == "fix":
             act_quantizer = QFixSymmetric(
                 nbits, init_clip_val=clip_val, align_zero=align_zero
-            )
-        elif qa_mode == "maxsym":
-            act_quantizer = Qmax(
-                nbits,
-                align_zero=True,
-                minmax=False,
-                extend_act_range=extend_act_range,
             )
         elif qa_mode == "pactsym":
             act_quantizer = PACT2Sym(
@@ -190,8 +194,6 @@ def get_activation_quantizer(
                     perToken=perToken,
                     emulate=True,
                 )
-        elif qa_mode == "pertokenmax":
-            act_quantizer = PerTokenMax(nbits)
         else:
             raise ValueError(f"unrecognized activation quantization mode {qa_mode}")
     else:  # swcap-compatible activation quantizers
@@ -266,7 +268,7 @@ def get_weight_quantizer(
     use_rot = False
     if "rot_" in qw_mode or "_rot" in qw_mode:
         use_rot = True
-        qw_mode.replace("rot_", "").replace("_rot", "")
+        qw_mode = qw_mode.replace("rot_", "").replace("_rot", "")
 
     weight_quantizer = None
     if not use_swcap:
@@ -3495,7 +3497,7 @@ class PerTokenMax(nn.Module):
         """
         For per-token activation quantization using abs().max() as scale,
         Zero is aligned so that the levels are symmetric around zero (lossing one level)
-        Since the token length is un-known before running, the quatnization is dynamic, meaning
+        Since the token length is un-known before running, the quantization is dynamic, meaning
         no trainable quantization scales and the scales are computed at run time.
         """
         super().__init__()
@@ -3507,6 +3509,42 @@ class PerTokenMax(nn.Module):
         scales.clamp_(min=1e-5).div_(levels)
         input_tensor.div_(scales).round_().mul_(scales)
         return input_tensor
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(num_bits={self.num_bits}, quantizer=)"
+
+
+class QMaxDynamic(nn.Module):
+    def __init__(self, num_bits, dim=-1):
+        """
+        For per-token or per-channel quantization using abs().max() as scale, usually for activation
+        and could be used for Qbmm M2 as well.
+        (reduce) dim = -1 -> abs() will output a column vector (if input is 2D) => per token
+                 dim = -2 -> per-channel
+        Zero is aligned so that the levels are symmetric around zero (lossing one level)
+        Since the token length is un-known before running, the quantizater can only calculate the
+        scales at the run times dynamically, meaning no trainable quantization scales is allowed.
+        (unless input seq length is always the same, not just padded to a fixed length.)
+        """
+        super().__init__()
+        self.num_bits = num_bits
+        self.levels = 2 ** (self.num_bits - 1) - 1
+        if isinstance(dim, str):
+            if "perCh" in dim or "per_channel" in dim:
+                dim = -2
+            elif "perToken" in dim or "per_token" in dim or "per_Token" in dim:
+                dim = -1
+        elif dim in [-1, -2]:
+            self.reduce_dim = dim
+        else:
+            raise ValueError(
+                f"Reduce dim can only be [-1, -2] or ['perCh', 'perToken'] but found {dim}"
+            )
+
+    def forward(self, input_tensor):
+        amax_dim = input_tensor.abs().max(dim=self.reduce_dim, keepdim=True)[0]
+        scales = amax_dim.clamp(min=1e-5).div(self.levels)
+        return input_tensor.div(scales).round().mul(scales)
 
     def __repr__(self):
         return f"{self.__class__.__name__}(num_bits={self.num_bits}, quantizer=)"
@@ -4585,7 +4623,7 @@ class MSEObserver(ObserverBase):
 
 class Qbypass(nn.Module):
     """
-    no quantization at all, straight-thru
+    No quantization at all, output the input_tensor directly.
     in place of lambda function when using nbits=32 and 16.
     to avoid issue when pickle (ie torch.save) of lambda
     (seems to be a problem only for DDP)

fms_mo/quant/rotation.py
@@ -18,62 +18,178 @@
 import torch
 
 # Local
-from fms_mo.utils.hadamard_util import matmul_hadU_cuda
+from fms_mo.utils.hadamard_util import matmul_hadU, matmul_hadU_cuda
 
 
 class RotQuantWrapper(torch.nn.Module):
     """Add a wrapper to fms-mo quantizers. Objects of this class could have two rotation tensors,
     and basic formula is:
 
-        self.quantizer(self.rot_left @ input_tensor @ self.rot_right)
+        quantizer(Rot_left @ input_tensor @ Rot_right)
 
-    NOTE rot_xxx could be optional, depending on whether it's for weights or activations.
-    For example, in SpinQuant QKV Linears will looks like (pseudo-code, "self" are not refering
-    to the same objects here):
-        qx = self.quantize_feature(x)                       # no rotation, just a normal quantizer
-        qw_q = self.quantize_weight(self.weight, R1_t)      # need left rotation only
-        qw_k = self.quantize_weight(sefl.weight, R1_t)
-        qw_v = self.quantize_weight(sefl.weight, R1_t, R2)  # need both left and right rotation
+    But Rot_xxx could be optional, depending on whether it's for weights or activations.
 
-        return F.linear(qx, qw, bias)
+    For weights, two possible use cases in SpinQuant are:
+        (A^-1 W) and (A^-1 W B).
+    Since linear.weight is already W^T and should stay as (rotated W)^T , these two cases will be
+        (A^-1 W)^T = W^T (A^-1)^T = W^T A, as A is a Hadamard matrix
+        (A^-1 W B)^T = B^T W^T A
+    ** Furthermore, depending on R1 is A (v_proj) or B (o_ and down_proj), computation could be
+        slightly different
+            if R1 is A (R_left):
+                calc W^T A first -> (W^T A)^T -> reshape -> *B -> .t() then ready for linear
+            else R1 is B (R_right):
+                calc B^T W^T first -> reshape -> *A -> ready for linear
 
-    for MLP down_proj
-        qx = self.quantize_feature(x, None, R4)             # for activation, should be x @ R
-        qw = self.quantize_weight(sefl.weight, R4_t, R1)
+    For activation (online rotation), it will always be (input_tensor @ R_right)
 
-        return F.linear(qx, qw, bias)
+    then    return F.linear(qx, qw, bias)
 
-    Also need to make sure self.R is pointing to a nn.Parameter() if training on R is needed.
+    NOTE
+    0. If online_full_had == False and self.R_left is None => do nothing, apply quantizer ONLY.
+    1. Make sure self.R is pointing to a nn.Parameter() if training on R is needed.
+    2. Because R is a ptr to a nn.Param tensor, it CANNOT store a "transposed" copy, hence the use
+        of self.transpose flags if needed.
     """
 
-    def __init__(self, quantizer, *args, **kwargs):
+    def __init__(self, quantizer=None, *args, **kwargs):
         self.online_full_had = kwargs.pop("online_full_had", None)
-        self.f32_had = kwargs.pop("f32_had", None)
+        self.compute_dtype = kwargs.pop("compute_dtype", torch.float64)
         super().__init__(*args, **kwargs)
         self.quantizer = quantizer
         self.R_left = None
         self.R_right = None
-        self.K_left = None  # if K_xxx > 1, R_xxx is a special had matrix
+        self.K_left = None
         self.K_right = None
+        self.R1_is_left = True  # see dosstring above
+        self.transpose_right = False  # this flag is for online rotation only
+        # if K_xxx == 1, use exact hadamard matrix. (R_xxx won't be needed). but if K > 1, R will
+        # be one of the 12 special had matrix. (they are stored in a binary file)
 
-    def forward(self, input_tensor):
-        org_dtype = input_tensor.dtype
+    def forward(self, inp):
+        org_dtype = inp.dtype
 
-        if self.online_full_had:
-            # online hadamard => rotation for activation. should be input_tensor @ R_right
-            # cannot be fused into W and no training, either.
-            if self.fp32_had:
-                input_tensor = input_tensor.float()
-            input_tensor = matmul_hadU_cuda(
-                input_tensor, self.R_right, self.K_right
+        if self.R_left is not None:
+            # Case 1: Weight rotation
+            #       as Activation rotation will only have R_right. If R_left exists for A =>
+            #       should have absorbed R_left for A into prev layer's W.
+            #       Hence, R_left is not None can only mean weight rotation, not online =>
+            #       could be either 1) R_left only or 2) both R_left and R_right.
+
+            in_feat, out_feat = inp.shape[-1], inp.shape[0]  # input is W^T (out, in)
+            if self.R1_is_left:
+                # for q, k, v, up, gate, calc W^T A first. see details in docstring
+                inp = inp.to(self.compute_dtype) @ self.R_left.to(self.compute_dtype)
+
+                if self.R_right is not None:
+                    had_dim = self.R_right.shape[0]
+                    inp = inp.t()  # (W^T A) ^T = A^T W, shape is (in, out)
+                    inp = inp.reshape(-1, out_feat // had_dim, had_dim)
+                    inp = inp.to(self.compute_dtype) @ self.R_right.to(
+                        self.compute_dtype
+                    )
+                    inp = inp.reshape((in_feat, out_feat)).t()
+
+            else:
+                assert self.R_right is not None, "R1_is_right but R_right is None."
+
+                # for o, down, calc B^T W^T first, where R1 is B
+                inp = self.R_right.t().to(self.compute_dtype) @ inp.to(
+                    self.compute_dtype
+                )
+                had_dim = self.R_left.shape[0]
+                inp = inp.t()  # this will be W, not W^T, i.e. (in, out)
+                w_shape = inp.shape
+                inp = inp.reshape(-1, in_feat // had_dim, had_dim)
+                inp = inp.to(self.compute_dtype) @ self.R_left.to(self.compute_dtype)
+                inp = inp.reshape((out_feat, in_feat))
+
+        elif self.R_right is not None or self.K_right == 1:
+            # Case 2: rotation for activation. should always be (inp @ R_right)
+            if self.online_full_had:
+                # Case 2-1: online, no training to R. When R_right is None (K==1), use exact size
+                if self.compute_dtype in [torch.float, torch.float64]:
+                    # follow SpinQuant paper, use no higher than fp32 for online had
+                    inp = inp.float()
+
+                # matmul_hadU_cuda already include 1/sqrt(shape[-1])
+                if self.transpose_right and self.R_right is not None:
+                    inp = matmul_hadU_cuda(inp, self.R_right.t(), self.K_right)
+                else:
+                    inp = matmul_hadU_cuda(inp, self.R_right, self.K_right)
+                    # inp = matmul_hadU(inp)
+            else:
+                # Case 2-2: offline (such as last R before lm_head)
+                if self.transpose_right:
+                    inp = inp.to(self.compute_dtype) @ self.R_right.t().to(
+                        self.compute_dtype
+                    )
+                else:
+                    inp = inp.to(self.compute_dtype) @ self.R_right.to(
+                        self.compute_dtype
+                    )
+
+        # Case 3: both R_left and R_right are None and K!=1=> No Rotation, apply quantizer if exist.
+
+        inp = inp.to(org_dtype)
+
+        if self.quantizer:
+            # with torch.no_grad():
+            inp = self.quantizer(inp)
+
+        return inp
+
+    def __repr__(self):
+        """Simplified repr for RotQuantizer. Shows name and nbits."""
+        repr_str = "Only("
+        if self.quantizer is not None:
+            repr_str = f"{self.quantizer.__class__.__name__}("
+
+        if self.R_left is not None or self.online_full_had:
+            # will do W or A rotation
+            repr_str = (
+                "Rot"
+                + repr_str
+                + f"{'' if self.R_left is None else 'Rl'},{'' if self.R_right is None else 'Rr'})"
+            )
+
+        return repr_str
+
+
+class EmbeddingRotWrapper(torch.nn.Module):
+    """Simply add a Rotation after input embeddings. original code looks like
+
+            input_embeds = self.embed_tokens(input_ids)
+
+        This wrapper will be:
+
+            input_embeds = self.embed_tokens(input_ids)
+            dtype = input_embeds.dtype
+            if self.R:
+                input_embeds = input_embeds @ self.R).to(dtype)
+            return input_embeds
+
+    Also need to make sure self.R is pointing to a nn.Parameter() if training on R is needed.
+    """
+
+    def __init__(self, emb, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.emb = emb
+        self.R = None
+        self.compute_dtype = torch.float64
+
+    def forward(self, inp_ids):
+        inp_embeds = self.emb(inp_ids)
+        org_dtype = inp_embeds.dtype
+        if self.R is not None:
+            inp_embeds = (
+                inp_embeds.to(self.compute_dtype) @ self.R.to(self.compute_dtype)
             ).to(org_dtype)
+        return inp_embeds
 
-            return input_tensor
-
-        # not online => rotation for weights, could be fused into W later.
-        if self.R_left:
-            input_tensor = self.R_left @ inp_tensor
-        if self.R_right:
-            inp_tensor = inp_tensor @ self.R_right
-
-        return inp_tensor
+    def __repr__(self):
+        """Simplified repr for RotEmb."""
+        repr_str = f"Rot{str(self.emb)}"
+        if self.R is not None:
+            repr_str.replace(")", ", Rr)")
+        return repr_str

fms_mo/utils/hadamard_util.py
@@ -21,10 +21,20 @@ Change original "text tensor implementation" into binaries for better efficiency
 sizes available in the safetensors file. [12, 20, 28, 36, 40, 44, 52, 60, 108, 140, 156, 172]
 """
 
+# Standard
+from pathlib import Path
+
 # Third Party
-from fast_hadamard_transform import hadamard_transform
+from fast_hadamard_transform import hadamard_transform  # pylint: disable=import-error
 from safetensors import safe_open
 import torch
+
+# TODO make sure it's a persistent cache so we don't need to load from file everytime
+cwd = Path(__file__).parent
+hadKs = {}
+with safe_open(cwd / "hadk.safetensors", framework="pt", device="cuda") as f:
+    for K_str in f.keys():  # K is a str
+        hadKs[K_str] = f.get_tensor(K_str)
 
 
 class HadamardTransform(torch.autograd.Function):
@@ -32,11 +42,11 @@ class HadamardTransform(torch.autograd.Function):
 
     # TODO seems redundant, insdie hadamard_transform(), backward is already handled...?
     @staticmethod
-    def forward(ctx, u):
+    def forward(_ctx, u):
         return hadamard_transform(u)
 
     @staticmethod
-    def backward(ctx, grad):
+    def backward(_ctx, grad):
         return hadamard_transform(grad)
 
 
@@ -44,15 +54,9 @@ def get_hadK(n, transpose=False):
     """Simplify the implementation and use binary tensors instead of text implementation."""
     for K in [172, 156, 140, 108, 60, 52, 44, 40, 36, 28, 20, 12]:
         if n % K == 0 and is_pow2(n // K):
-            with safe_open("hadk.safetensors", framework="pt") as f:
-                assert (
-                    str(K) in f.keys()
-                ), f"Special size Hadamard {K} does not exist in the file."
-                hadK = f.get_tensor(str(K))
-
+            hadK = hadKs[str(K)]
             if transpose:
                 hadK = hadK.T
-
             break
 
     if hadK is None:
@@ -67,35 +71,39 @@ def get_hadK(n, transpose=False):
 
 
 def matmul_hadU(X, transpose=False):
+    """Borrowed from SpinQuant."""
     n = X.shape[-1]
     hadK, K = get_hadK(n, transpose)
-    input = X.clone().view(-1, n, 1)
-    output = input.clone()
-    while input.shape[1] > K:
-        input = input.view(input.shape[0], input.shape[1] // 2, 2, input.shape[2])
-        output = output.view(input.shape)
-        output[:, :, 0, :] = input[:, :, 0, :] + input[:, :, 1, :]
-        output[:, :, 1, :] = input[:, :, 0, :] - input[:, :, 1, :]
-        output = output.view(input.shape[0], input.shape[1], -1)
-        (input, output) = (output, input)
+    input_ = X.clone().view(-1, n, 1)
+    output = input_.clone()
+    while input_.shape[1] > K:
+        input_ = input_.view(input_.shape[0], input_.shape[1] // 2, 2, input_.shape[2])
+        output = output.view(input_.shape)
+        output[:, :, 0, :] = input_[:, :, 0, :] + input_[:, :, 1, :]
+        output[:, :, 1, :] = input_[:, :, 0, :] - input_[:, :, 1, :]
+        output = output.view(input_.shape[0], input_.shape[1], -1)
+        (input_, output) = (output, input_)
     del output
 
     if K > 1:
         # Do not explicitly repeat - OOM
-        # input = torch.bmm(
-        #     hadK.repeat(len(input), 1, 1).to(input.device).to(input.dtype), input)
+        # input_ = torch.bmm(
+        #     hadK.repeat(len(input_), 1, 1).to(input_.device).to(input_.dtype), input_)
         # Use bcast instead
-        input = hadK.view(1, K, K).to(input) @ input
+        input_ = hadK.view(1, K, K).to(input_) @ input_
 
-    return input.view(X.shape) / torch.tensor(n).sqrt()
+    return input_.view(X.shape) / torch.tensor(n).sqrt()
 
 
 def matmul_hadUt(X):
+    """Borrowed from SpinQuant."""
     return matmul_hadU(X, transpose=True)
 
 
 def random_hadamard_matrix(size, device):
-    # See https://cornell-relaxml.github.io/quip-sharp/ , Section "Randomized Hadamard Transformation"
+    """Borrowed from SpinQuant."""
+    # See https://cornell-relaxml.github.io/quip-sharp/
+    # Section "Randomized Hadamard Transformation"
     Q = torch.randint(low=0, high=2, size=(size,)).to(torch.float64)
     Q = Q * 2 - 1
     Q = torch.diag(Q)
@@ -103,28 +111,31 @@ def random_hadamard_matrix(size, device):
 
 
 def hadamard_matrix(size, device):
-    # See https://cornell-relaxml.github.io/quip-sharp/ , Section "Randomized Hadamard Transformation"
+    """Borrowed from SpinQuant."""
     Q = torch.eye(size)
     return matmul_hadU(Q).to(device)
 
 
 def matmul_hadU_cuda(X, hadK, K):
+    """Borrowed from SpinQuant."""
     n = X.shape[-1]
     if K == 1:
         return HadamardTransform.apply(X.contiguous()) / torch.tensor(n).sqrt()
     # if transpose:
     #     hadK = hadK.T.contiguous()
-    input = X.view(-1, K, n // K)
-    input = HadamardTransform.apply(input.contiguous()) / torch.tensor(n).sqrt()
-    input = hadK.to(input.device).to(input.dtype) @ input
-    return input.reshape(X.shape)
+    input_ = X.view(-1, K, n // K)
+    input_ = HadamardTransform.apply(input_.contiguous()) / torch.tensor(n).sqrt()
+    input_ = hadK.to(input_.device).to(input_.dtype) @ input_
+    return input_.reshape(X.shape)
 
 
-def matmul_hadUt_cuda(X, hadK, K):
-    return matmul_hadU_cuda(X, hadK, K, transpose=True)
+# def matmul_hadUt_cuda(X, hadK, K):
+#     """Borrowed from SpinQuant."""
+#     return matmul_hadU_cuda(X, hadK, K, transpose=True)
 
 
 def apply_exact_had_to_linear(module, had_dim=-1, output=False, R2=None):
+    """Borrowed from SpinQuant."""
     assert isinstance(module, torch.nn.Linear)
     in_features, out_features = module.in_features, module.out_features
 
@@ -163,6 +174,7 @@ def apply_exact_had_to_linear(module, had_dim=-1, output=False, R2=None):
 
 
 def is_pow2(n):
+    """Borrowed from SpinQuant."""
     return (n & (n - 1) == 0) and (n > 0)
 
 